af735b3712ce39ecd62888b7789d912359575923
NAINIKAGOSWAMI <74713715+NAINIKAGOSWAMI@users.noreply.github.com>
2021-04-05 16:48:29 +0530

    Add files via upload

---
+++ Contestant.js
@@ -0,0 +1,37 @@
+class Contestant {
+    constructor(){
+  this.index = null;
+  this.distance = 0;
+  this.name = null;
+    }
+  
+    getCount(){
+      var ContestantCountRef = database.ref('contestantCount');
+     ContestantCountRef.on("value",function(data){
+       ContestantCount = data.val();
+      })
+    }
+  
+    updateCount(count){
+      database.ref('/').update({
+       ContestantCount: count
+      });
+    }
+  
+    update(){
+      varContestantIndex = "contestants/contestant" + this.index;
+      database.ref(playerIndex).set({
+        name:this.name,
+        distance :this.distance
+      });
+    }
+    static getPlayerInfo(){
+      varContestantInfoRef  =database.ref('contestants');
+     ContestantInfoRef.on("value",(data)=>{
+        allContestants = data.val();
+      })
+    }
+  
+    
+  }
+  
