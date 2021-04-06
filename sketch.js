6d7ef788bba993f2a62620526a1fe956b3a86646
NAINIKAGOSWAMI <74713715+NAINIKAGOSWAMI@users.noreply.github.com>
2021-04-06 13:54:35 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,18 @@
-var canva;
+var canvas, backgroundImage;
+
 var gameState = 0;
 var contestantCount;
-var contestant
+var allContestants;
+var answer;
 var database;
-var quiz;
-var question;
+
+var question, contestant, quiz;
 
 
 function setup(){
   canvas = createCanvas(850,400);
   database = firebase.database();
-  
-  quiz= new Quiz();
+  quiz = new Quiz();
   quiz.getState();
   quiz.start();
 }
@@ -19,6 +20,11 @@ function setup(){
 
 function draw(){
   background("pink");
-
-  
+  if(contestantCount === 2){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
 }
