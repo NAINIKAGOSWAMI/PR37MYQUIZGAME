8a40fcd0621329a5c3e3d73c627fc67734329cf8
NAINIKAGOSWAMI <74713715+NAINIKAGOSWAMI@users.noreply.github.com>
2021-04-07 22:58:31 +0530

    Add files via upload

--- js/Quiz.js
+++ js/Quiz.js
@@ -1,28 +1,65 @@
 class Quiz {
-    constructor(){}
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  
+  play(){ 
     
-    getState(){
-      var gameStateRef  = database.ref('gameState');
-      gameStateRef.on("value",function(data){
-         gameState = data.val();
-      })
+    Contestant.getContestantInfo();
+
+    var i =createElemant('h2');
+    i.html("RESULT");
+    i.position(100,0);
+    background("yellow");
+
+    var y =270;
+    if(allContestants!==undefined){
+      fill("blue")
+      textSize(20);
+      text("Contestants who are right are highlighted in green",150,230);
      
     }
-    update(state){
-        database.ref('/').update({
-          gameState: state
-        });
+    for(var plr in allContestants){
+      y = y+20;
+      var correctAns="2";
+      if(correctAns==allContestants[plr].answer){
+        fill("green");
+        text(allContestants[plr].name+"You are right",150,y);
+       
+        
       }
-      async start(){
-        if(gameState === 0){
-          contestant = new Contestant();
-          var contestantCountRef = await database.ref("contestantCount").once("value")
-          if(contestantCountRef.exists()){
-            contestantCount = contestantCountRef.val()
-            contestant.getCount();
+      else{
+        fill("red")
+        text(allContestants[plr].name+"You are wrong",150,y);
+      }
     }
-    question = new Question();
-    question.display();
-}
-}
+    
+  }
+
 }
